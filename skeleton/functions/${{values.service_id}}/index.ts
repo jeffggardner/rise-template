5f2e2e62c0cf16bf9bf365a74ce45418ec9e80b5
theJeffG <jeffggardner@gmail.com>
2022-12-06 16:02:44 -0700

    initial commit

--- skeleton/functions/${{values.service_id}}/index.ts
+++ skeleton/functions/${{values.service_id}}/index.ts
@@ -2,11 +2,11 @@ import shared from '@republicservicesrepository/dops-shared';
 const { logger, wrapper } = shared;
 const { Service } = wrapper
 
-import { ${{values.service_id}}Request } from './model';
-import ${{values.service_id}}Schema from './schema';
-import { handlerFunction } from './${{service_name}}.function';
+import { ${{values.service_name}}Request } from './model';
+import ${{values.service_name}}Schema from './schema';
+import { handlerFunction } from './${{values.service_id}}.function';
 
-async function handlerWrapper(request: ${{values.service_name}}Request ): Promise< ${{values.service_name}}Response {
+async function handlerWrapper(request: ${{values.service_name}}Request ): Promise< ${{values.service_name}}Response > {
 
   const transactionId = request.headers.TransactionID;
   logger.info(`begin transactionId ${transactionId}`);
@@ -15,4 +15,4 @@ async function handlerWrapper(request: ${{values.service_name}}Request ): Promis
   return handlerFunction(request);
 }
 
-export const { handler } = new Service(handlerWrapper, ${{values.service_id}}Schema);
+export const { handler } = new Service(handlerWrapper, ${{values.service_name}}Schema);
